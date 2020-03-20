67d870d5d70abb366399c1438a3f7554eec9ef7b
jessicaelee <jessica.e.lee.93@gmail.com>
2020-03-19 17:02:06 -0700

    add

--- prob-divide-conquer/count-zeroes.js
+++ prob-divide-conquer/count-zeroes.js
@@ -1,7 +1,19 @@
-Time Complexity: O(log N)
+// Time Complexity: O(log N)
 
-function countZeroes() {
+function countZeroes(arr) {
+  let midIdx = Math.floor(arr.length / 2);
+  let previousMidIdx = null;
 
+  while (midIdx !== previousMidIdx) {
+    previousMidIdx = midIdx;
+    if (arr[midIdx] === 0) {
+      midIdx = Math.floor(midIdx / 2);
+    } else if (arr[midIdx] === 1) {
+      midIdx = Math.floor(previousMidIdx)
+    }
+
+  }
+  return arr.length - midIdx;
 }
 
 module.exports = countZeroes
